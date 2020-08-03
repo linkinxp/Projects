fa6ffb4a5021cbd9a5bda2688f054e5a3842313b
Jorge Suarez <66175003+linkinxp@users.noreply.github.com>
2020-08-03 13:45:25 -0400

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,1 +1,21 @@
-bash scriptsd
+$isitcharging = Get-WmiObject win32_Battery | Select-Object -ExpandProperty BatteryStatus
+$time = Get-Date
+
+if ($isitcharging -eq "2") 
+{
+$currentvalue = Get-Ciminstance -Namespace root/WMI -ClassName WmiMonitorBrightness | Select-Object -ExpandProperty CurrentBrightness
+(Get-WmiObject -Namespace root/WMI -Class WmiMonitorBrightnessMethods).WmiSetBrightness(1,85)
+"Laptop is charging --- Current brightness is " + $currentvalue + " brightness set to 85"  + " at " + $time | Out-File -Append -FilePath C:\bin\Logs\Brightness.log
+}
+else
+{
+$setvalue = "35"
+$currentvalue = Get-Ciminstance -Namespace root/WMI -ClassName WmiMonitorBrightness | Select-Object -ExpandProperty CurrentBrightness
+(Get-WmiObject -Namespace root/WMI -Class WmiMonitorBrightnessMethods).WmiSetBrightness(1,$setvalue)
+if ($setvalue -eq $currentvalue){
+"Laptop is not charging --- Current brightness is " + $currentvalue + " brightness remains the same" + " at " + $time | Out-File -Append -FilePath C:\bin\Logs\Brightness.log
+}
+else{
+"Laptop is not charging --- Current brightness is " + $currentvalue + " brightness set to $setvalue" + " at " + $time | Out-File -Append -FilePath C:\bin\Logs\Brightness.log
+}}
+exit
